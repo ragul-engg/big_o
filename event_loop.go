d938c9a84b17d7dbb90d8897a9e5a3e38f68a98a
ragul-engg <ragulshanmugarajan21@gmail.com>
2025-04-18 09:52:30 +0530

    Make maximum memory size for allowing writes to 2.5GB

--- event_loop.go
+++ event_loop.go
@@ -15,7 +15,7 @@ Flow:
 		Request comes -> block the channel for the go routine handling that API response till we fetch and decode for that,
 		perform gets from all across the other pods and wait till we get the data through the shared channel
 */
-const BYTES_IN_GB uintptr = 1_00_00_00_000
+const BYTES_IN_GB uintptr = 2_50_00_00_000
 
 var totalSize uintptr
 
